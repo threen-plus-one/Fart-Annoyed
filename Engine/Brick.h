5e5d6e0c31d56f0f4746849e75affd78598e5718
ThreeNPlusOne <ThreeNPlusOne@DESKTOP-48S66SI>
2017-02-01 20:05:01 +1100

    expanded Brick class a bit

--- Engine/Brick.h
+++ Engine/Brick.h
@@ -3,15 +3,18 @@
 #include "RectF.h"
 #include "Colors.h"
 #include "Graphics.h"
+#include "Ball.h"
 
 class Brick
 {
 	RectF rect;
 	Color color;
 
-	bool destroyed;
+	bool destroyed = false;
 
 public:
 	Brick( const RectF& rect,Color color );
 	void Draw( Graphics& gfx ) const;
+
+	bool DoBallCollision( Ball& ball );
 };
