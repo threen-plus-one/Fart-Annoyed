ae303ab2d62b86c4429d60684c39bd77a33060ad
ThreeNPlusOne <ThreeNPlusOne@DESKTOP-48S66SI>
2017-02-02 18:45:38 +1100

    added Paddle class (that's a paddlin')

---
+++ Engine/Paddle.cpp
@@ -0,0 +1,57 @@
+#include "Paddle.h"
+
+Paddle::Paddle( const Vec2& pos_in,float halfWidth_in,float halfHeight_in )
+	:
+	pos( pos_in ),
+	halfWidth( halfWidth_in ),
+	halfHeight( halfHeight_in )
+{
+}
+
+void Paddle::Update( Keyboard& kbd,float dt )
+{
+	if( kbd.KeyIsPressed( VK_LEFT ) )
+	{
+		pos.x -= SPEED * dt;
+	}
+
+	if( kbd.KeyIsPressed( VK_RIGHT ) )
+	{
+		pos.x += SPEED * dt;
+	}
+}
+
+void Paddle::Draw( Graphics& gfx ) const
+{
+	RectF rect = GetBounds();
+	gfx.DrawRect( rect,color );
+}
+
+bool Paddle::DoBallCollision( Ball& ball ) const
+{
+	if( GetBounds().Intersects( ball.GetBounds ) )
+	{
+		ball.ReboundY();
+		return true;
+	}
+	return false;
+}
+
+void Paddle::DoWallCollision( const RectF& walls )
+{
+	const RectF rect = GetBounds();
+
+	if( rect.left < walls.left )
+	{
+		pos.x += walls.left - rect.left;
+	}
+	else if( rect.right > walls.right )
+	{
+		pos.x -= rect.right - walls.right;
+	}
+}
+
+RectF Paddle::GetBounds() const
+{
+	return RectF::FromCentre( pos,halfWidth,halfHeight );
+}
---
+++ Engine/Paddle.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Vec2.h"
+#include "RectF.h"
+#include "Colors.h"
+#include "Keyboard.h"
+#include "Ball.h"
+
+class Paddle
+{
+	static constexpr float SPEED = 200.0f;
+
+	Vec2 pos;
+	float halfWidth;
+	float halfHeight;
+
+	Color color = Colors::White;
+
+public:
+	Paddle( const Vec2& pos,float halfWidth,float halfHeight );
+
+	void Update( Keyboard& kbd,float dt );
+	void Draw( Graphics& gfx ) const;
+
+	bool DoBallCollision( Ball& ball ) const;
+	void DoWallCollision( const RectF& walls );
+
+	RectF GetBounds() const;
+};
