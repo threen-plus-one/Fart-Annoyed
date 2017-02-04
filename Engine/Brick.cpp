43b4d08f01288cd197a1027d7759e3104107ee55
ThreeNPlusOne <ThreeNPlusOne@DESKTOP-48S66SI>
2017-02-04 18:49:21 +1100

    better brick collision #2

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -18,7 +18,11 @@ void Brick::ExcecuteBallCollision( Ball& ball )
 	assert( CheckBallCollision( ball ) );
 
 	const Vec2 ballPos = ball.GetPosition();
-	if( ballPos.x >= rect.left && ballPos.x <= rect.right )
+	if( std::signbit( ball.GetVelocity().x ) == std::signbit( ( ballPos - rect.GetCentre() ).x ) )
+	{
+		ball.ReboundY();
+	}
+	else if( ballPos.x >= rect.left && ballPos.x <= rect.right )
 	{
 		ball.ReboundY();
 	}
